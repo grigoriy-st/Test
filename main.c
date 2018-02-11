10de79f51b8bb3edb16e0e174b7bfebfdae078ad
Grigoriy <gregorey060@gmail.com>
2018-02-11 11:31:40 +0300

    Commit

---
+++ main.c
@@ -0,0 +1,24 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  main.c
+ *
+ *    Description:  This is first file 
+ *
+ *        Version:  1.0
+ *        Created:  11.02.2018 5:37:20
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Grigoriy Starostin, 
+ *   Organization:  Self-development club
+ *
+ * =====================================================================================
+ */
+#include <stdlib.h>
+
+int main(int argc, char * argv[])
+{
+    printf("%s", "Hello, world");
+    return 0;
+}
